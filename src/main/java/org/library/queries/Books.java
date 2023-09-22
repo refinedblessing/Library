dcbb8703093f13659e270bfd7ab05fd238953295
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2023-09-21 20:27:55 -0700

    Set up queries for performing CRUD operations on Books table

--- src/main/java/org/library/queries/Books.java
+++ src/main/java/org/library/queries/Books.java
@@ -1,4 +1,19 @@
 package org.library.queries;
 
-public class Books {
+public enum Books {
+    GET_ALL_BOOKS("SELECT * FROM books"),
+    GET_BOOK_BY_ISBN("SELECT * FROM books WHERE isbn = ?"),
+    GET_BOOK_BY_ID("SELECT * FROM books WHERE id = ?"),
+    DELETE_BOOK("DELETE FROM books WHERE id = ?"),
+    SAVE_BOOK("INSERT INTO books(isbn, name) VALUES(?, ?)");
+
+    private final String query;
+
+    Books(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
 }
